7a25204611bf6833dd8736cf3c78fb925a4aee31
Kelli Libin <km.putnam2@gmail.com>
2023-03-17 14:48:53 -0600

    implement monaco

---
+++ src/components/code-editor.tsx
@@ -0,0 +1,64 @@
+import { useRef } from "react";
+import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
+
+interface CodeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const editorRef = useRef<any>();
+  //called whenever displayed on screen. first arg is function we can use to get current value out of the editor, gets value of first mount
+  //second argument is a reference to the editor itself. can use monacoEditor to be told whenever user changes the contents
+  //get value only for retrieving value
+  const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    editorRef.current = monacoEditor;
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
+
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+  };
+
+  const onFormatClick = () => {
+    //get current value from editor
+    const unformatted = editorRef.current.getModel().getValue();
+    //format the value
+    const formatted = prettier.format(unformatted, {
+      parser: "babel",
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    });
+    //set the formatted value back in the editor
+    editorRef.current.setValue(formatted)
+  };
+
+  return (
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+      <MonacoEditor
+        editorDidMount={onEditorDidMount}
+        value={initialValue}
+        language="javascript"
+        height="300px"
+        theme="dark"
+        options={{
+          wordWrap: "on",
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </div>
+  );
+};
+
+export default CodeEditor;
