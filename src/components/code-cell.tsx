445f33489fe11a0d679266a746ab3a905c962245
Kelli Libin <km.putnam2@gmail.com>
2023-03-22 14:39:00 -0600

    debouncing for editor and preview

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //function that bundles
 import bundle from "../bundler";
@@ -12,12 +12,28 @@ import Resizable from "./resizable";
 const CodeCell = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
+  const [error, setError] = useState("")
 
   //the bundle step. the transpile func is async, so make sure this is async
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
+
+  //wait for 1 ish seconds without any updates to state, if that happens, we wat to run bundling logic. this is
+  //called debouncing: we want a piece of code to run as much as possible until a certain amount of time passes, then we run some function
+  //so, we are: user types into editor, first time they do we update input state. also set a timer in the code where it bundles user code.
+  //if a user then types into the editor sooner than that 1 second, again update, but cancel the previous timer so we don't execute that
+  //first bundling logic. repeat this process until 1 second goes by without us cancelling the timer, then the bundling happens
+
+  useEffect(() => {
+   const timer =  setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output.code);
+      setError(output.err)
+    }, 1000);
+
+    //remember, return funcs (cleanup) will run every time useeffect is called. execute logic to cancel previous timer
+    return () => {
+        clearTimeout(timer)
+    }
+  }, [input]);
 
   return (
     <Resizable direction="vertical">
@@ -28,7 +44,7 @@ const CodeCell = () => {
             onChange={(value) => setInput(value)}
           />
         </Resizable>
-        <Preview code={code} />
+        <Preview code={code} bundleStatus={error}/>
       </div>
     </Resizable>
   );
