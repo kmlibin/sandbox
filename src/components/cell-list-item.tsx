ee18d032a8fdb090fedf6b881796278aa840ffab
Kelli Libin <km.putnam2@gmail.com>
2023-03-28 15:42:36 -0600

    create and render the action bar

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,24 +1,27 @@
 import { Cell } from "../state";
+import ActionBar from "./action-bar";
 
 //components
 import CodeCell from "./code-cell";
 import TextEditor from "./text-editor";
 
 interface CellListItemProps {
-    cell: Cell
+  cell: Cell;
 }
 
-const CellListItem: React.FC<CellListItemProps> = ({cell}) => {
-    let child: JSX.Element;
-    if (cell.type === 'code') {
-        child = <CodeCell cell={cell} />
-    } else {
-        child = <TextEditor />
-    }
-    return (
-<div>
-    {child}
-</div>
-)};
+const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
+  let child: JSX.Element;
+  if (cell.type === "code") {
+    child = <CodeCell cell={cell} />;
+  } else {
+    child = <TextEditor cell={cell} />;
+  }
+  return (
+    <div>
+      <ActionBar id={cell.id} />
+      {child}
+    </div>
+  );
+};
 
-export default CellListItem
+export default CellListItem;
