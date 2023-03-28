b9fbf690dc5a335527fd1e12623c03f631d64a88
Kelli Libin <km.putnam2@gmail.com>
2023-03-28 16:08:52 -0600

    format, style buttons, add font awesome

--- src/components/action-bar.tsx
+++ src/components/action-bar.tsx
@@ -1,18 +1,41 @@
 import { useActions } from "../hooks/useActions";
 
+import "./action-bar.css"
+
 interface ActionBarProps {
-    id: string;
+  id: string;
 }
 
-const ActionBar: React.FC<ActionBarProps> = ({id}) => {
-    const {moveCell, deleteCell} = useActions();
-    return (
-        <div>
-            <button onClick= {() => moveCell(id, 'up')}>up</button>
-            <button onClick = {() => moveCell(id, 'down')}>down</button>
-            <button onClick= {() => deleteCell(id)}>delete</button>
-        </div>
-    )
-}
+const ActionBar: React.FC<ActionBarProps> = ({ id }) => {
+  const { moveCell, deleteCell } = useActions();
+  return (
+    <div className = "action-bar">
+      <button
+        className="button is-primary is-small"
+        onClick={() => moveCell(id, "up")}
+      >
+        <span className="icon">
+          <i className="fas fa-arrow-up" />
+        </span>
+      </button>
+      <button
+        className="button is-primary is-small"
+        onClick={() => moveCell(id, "down")}
+      >
+        <span className="icon">
+          <i className="fas fa-arrow-down" />
+        </span>
+      </button>
+      <button
+        className="button is-primary is-small"
+        onClick={() => deleteCell(id)}
+      >
+        <span className="icon">
+          <i className="fas fa-times" />
+        </span>
+      </button>
+    </div>
+  );
+};
 
 export default ActionBar;
