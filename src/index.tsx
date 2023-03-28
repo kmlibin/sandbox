fe9005270d4df136c445eeb98146845c2df2b899
Kelli Libin <km.putnam2@gmail.com>
2023-03-28 14:28:34 -0600

    render celllist

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,17 @@ import ReactDOM from "react-dom";
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./state";
-
-// import CodeCell from "./components/code-cell";
-import TextEditor from "./components/text-editor";
+import CellList from "./components/cell-list";
 
 //libraries
 import "bulmaswatch/superhero/bulmaswatch.min.css";
+
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <TextEditor />
-        {/* <CodeCell /> */}
+        <CellList />
       </div>
     </Provider>
   );
