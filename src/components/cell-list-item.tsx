6682342ced3a46c0b92ca1cda7f210586ac30d13
Kelli Libin <km.putnam2@gmail.com>
2023-03-28 15:05:34 -0600

    decide whether to render code or textcell

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,13 +1,23 @@
 import { Cell } from "../state";
 
+//components
+import CodeCell from "./code-cell";
+import TextEditor from "./text-editor";
+
 interface CellListItemProps {
     cell: Cell
 }
 
 const CellListItem: React.FC<CellListItemProps> = ({cell}) => {
+    let child: JSX.Element;
+    if (cell.type === 'code') {
+        child = <CodeCell cell={cell} />
+    } else {
+        child = <TextEditor />
+    }
     return (
 <div>
-    {cell.id}
+    {child}
 </div>
 )};
 
