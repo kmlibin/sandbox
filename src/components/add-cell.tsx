79cd54b885cad6a53ddfe2b5e7bb560e833ba363
Kelli Libin <km.putnam2@gmail.com>
2023-03-29 11:32:48 -0600

    insert cell before switch to insert cell after

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -2,18 +2,18 @@ import { useActions } from "../hooks/useActions";
 import "./add-cell.css";
 
 interface AddCellProps {
-  nextCellId: string | null;
+  prevCellId: string | null;
   forceVisible?: boolean;
 }
 
-const AddCell: React.FC<AddCellProps> = ({ nextCellId, forceVisible }) => {
-  const { insertCellBefore } = useActions();
+const AddCell: React.FC<AddCellProps> = ({ prevCellId, forceVisible }) => {
+  const { insertCellAfter } = useActions();
   return (
     <div className={`add-cell ${forceVisible && "force-visible"}`}>
       <div className="add-buttons">
         <button
           className="button is-rounded is-primary is-small"
-          onClick={() => insertCellBefore(nextCellId, "code")}
+          onClick={() => insertCellAfter(prevCellId, "code")}
         >
           <span className="icon is-small">
             <i className="fas fa-plus" />
@@ -22,7 +22,7 @@ const AddCell: React.FC<AddCellProps> = ({ nextCellId, forceVisible }) => {
         </button>
         <button
           className="button is-rounded is-primary is-small"
-          onClick={() => insertCellBefore(nextCellId, "text")}
+          onClick={() => insertCellAfter(prevCellId, "text")}
         >
           <span className="icon is-small">
             <i className="fas fa-plus" />
