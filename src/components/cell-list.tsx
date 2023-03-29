6aa86ca4480aea774446c5c1c0cbb8098e1724ca
Kelli Libin <km.putnam2@gmail.com>
2023-03-29 10:13:18 -0600

    render add cell component

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,18 +1,31 @@
+import { Fragment } from "react";
 import { useTypedSelector } from "../hooks/use-typed-selector";
+import AddCell from "./add-cell";
 
 import CellListItem from "./cell-list-item";
 
 const CellList: React.FC = () => {
-    //comes from our reducers
+  //selector. comes from our reducers
   const cells = useTypedSelector(({ cells: { order, data } }) => {
     //get all of our cells in an ordered list. we return each id from the order array, then we can look up data in the appropriate order
     return order.map((id) => {
       return data[id];
     });
   });
-  console.log(cells)
-  const renderedCells = cells.map((cell) => <CellListItem key={cell.id} cell = {cell}/>);
-  return <div>{renderedCells}</div>;
+  console.log(cells);
+  const renderedCells = cells.map((cell) => (
+    <Fragment key={cell.id}>
+      <AddCell nextCellId={cell.id} />
+      <CellListItem cell={cell} />
+    </Fragment>
+  ));
+
+  return (
+    <div>
+      {renderedCells}
+      <AddCell nextCellId={null} />
+    </div>
+  );
 };
 
 export default CellList;
