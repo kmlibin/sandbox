bce5d763247e15beb15e7e9b2fe0dabce3ca5ba9
Kelli Libin <km.putnam2@gmail.com>
2023-03-29 14:14:45 -0600

    setting up bundle actiontypes, creators, reducer

---
+++ src/state/reducers/bundlesReducer.ts
@@ -0,0 +1,31 @@
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+import { Cell } from "../cell";
+
+//import immer
+import produce from "immer";
+
+interface BundleState {
+  [key: string]: {
+    loading: boolean;
+    code: string;
+    err: string;
+  };
+}
+
+const initialState: BundleState = {};
+
+const reducer = produce(
+  (state: BundleState = initialState, action: Action): BundleState => {
+    switch (action.type) {
+      case ActionType.BUNDLE_START:
+        return state;
+      case ActionType.BUNDLE_COMPLETE:
+        return state;
+      default:
+        return state;
+    }
+  }
+);
+
+export default reducer;
