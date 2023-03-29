61c256edb0a71f6890924ef064949cdcf568cde2
Kelli Libin <km.putnam2@gmail.com>
2023-03-29 16:11:16 -0600

    local state bundling to redux

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,8 +1,4 @@
-import ReactDOM from "react-dom";
-import React, { useState, useEffect } from "react";
-
-//function that bundles
-import bundle from "../bundler";
+import React, { useEffect } from "react";
 
 //components
 import CodeEditor from "./code-editor";
@@ -10,15 +6,16 @@ import Preview from "./preview";
 import Resizable from "./resizable";
 import { Cell } from "../state";
 import { useActions } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/use-typed-selector";
+
+import "./code-cell.css";
 
 interface CodeCellProps {
   cell: Cell;
 }
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
-  const [code, setCode] = useState("");
-  const [error, setError] = useState("");
-  const { updateCell } = useActions();
-  //the bundle step. the transpile func is async, so make sure this is async
+  const { updateCell, createBundle } = useActions();
+  const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
   //wait for 1 ish seconds without any updates to state, if that happens, we wat to run bundling logic. this is
   //called debouncing: we want a piece of code to run as much as possible until a certain amount of time passes, then we run some function
@@ -27,28 +24,48 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   //first bundling logic. repeat this process until 1 second goes by without us cancelling the timer, then the bundling happens
 
   useEffect(() => {
+    //this is essentially so that first load doesn't take 1 s, and it loads immediately
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
     const timer = setTimeout(async () => {
-      const output = await bundle(cell.content);
-      setCode(output.code);
-      setError(output.err);
+      createBundle(cell.id, cell.content);
     }, 1000);
 
     //remember, return funcs (cleanup) will run every time useeffect is called. execute logic to cancel previous timer
     return () => {
       clearTimeout(timer);
-    };
-  }, [cell.content]);
+    }; //createBundle...only add if declared inside of func or recieved as prop
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cell.content, cell.id, createBundle]);
 
   return (
     <Resizable direction="vertical">
-      <div style={{ height: "calc(100% - 10px)", display: "flex", flexDirection: "row" }}>
+      <div
+        style={{
+          height: "calc(100% - 10px)",
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
         <Resizable direction="horizontal">
           <CodeEditor
             initialValue={cell.content}
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        <Preview code={code} bundleStatus={error} />
+        <div className="progress-wrapper">
+          {!bundle || bundle.loading ? (
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">
+                Loading
+              </progress>
+            </div>
+          ) : (
+            <Preview code={bundle.code} bundleStatus={bundle.err} />
+          )}
+        </div>
       </div>
     </Resizable>
   );
